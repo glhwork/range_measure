083b69100966697deddfc9e8646e53bc603a6304
GLINHAI <guilinhai950419@gmail.com>
2018-10-29 11:21:43 +0800

    delete the verbose

--- src/order_send.cpp
+++ src/order_send.cpp
@@ -19,33 +19,10 @@ int main(int argc, char** argv) {
   order.data.push_back(0x06);
   order.data.push_back(0x02);
   order.data.push_back(0x78);
-  std::vector<int> v;
-  int v1 = 0x80;
-  int v2 = 0x06;
-  int v3 = 0x02;
-  int v4 = 0x78;
-
-  v.push_back(v1);
-  v.push_back(v2);
-  v.push_back(v3);
-  v.push_back(v4);
-
-  
-  // for (size_t i = 0; i < v.size(); i++) {
-  // 	std::stringstream ss;
-  // 	ss << std::hex << v[i];
-  // 	std::string s_tmp;
-  // 	ss >> s_tmp;
-  // 	order.data = order.data + s_tmp + " ";
-  // }
-  // for (size_t i = 0; i < v.size(); i++) {
-  // 	order.data = order.data + v[i] + " ";
-  // }
-  // order.data = order.data + "\n";
 
   ros::Rate r(1);
   while(ros::ok()) {
-  	order_pub.publish(order);
+    order_pub.publish(order);
     ros::spinOnce();
     r.sleep();
   }
